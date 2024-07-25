284f069262e835f66388cb7adf7b54fe6ce7cba8
CodeWithAkhil01 <142796641+CodeWithAkhil01@users.noreply.github.com>
2024-07-25 16:22:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ let voiceSelect = document.querySelector("select");
 
 // for diff voices options
 window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices(); // get voices will provide all the available voices in the device
+  voices = window.speechSynthesis.getVoices(); 
   voicee.voice = voices[10]; // getVoices() retrieve or get all available voices and stores them in the voices array.
-  // 11 pe girl voice h uske liye defalut voice 2 set ki h
+  
 
   voices.forEach(
     (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
   );
 };
 
-// change event on dropdown.. dropdown m kisi b voice option p click krne p voice change hoo
+
 voiceSelect.addEventListener("change", () => {
   voicee.voice = voices[voiceSelect.value];
 });
